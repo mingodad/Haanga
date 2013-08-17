d17bd4f606ee8a6618c28badc431d04fb4b70ba8
mingodad <mingodad@gmail.com>
2013-08-17 18:21:36 +0100

    Make Haanga reentrant, dynamic instead of static.

--- examples/template_runtime.php
+++ examples/template_runtime.php
@@ -1,8 +1,12 @@
 <?php
+$time = microtime(TRUE);
+$mem = memory_get_usage();
 
 require "../lib/Haanga.php";
 
-$fnc = Haanga::compile(<<<EOT
+$my_haanga = new Haanga();
+
+$fnc = $my_haanga->compile(<<<EOT
     <h1>{{foobar}}{{    foobar  }}</h1>
 
     Este template será compilado a una función PHP ({{foo|default:foobar}})
@@ -13,3 +17,5 @@ EOT
 
 $fnc(array("foobar" => 'hola', 'foo' => '.I.'), FALSE /* print it */);
 $fnc(array("foobar" => 'chau'), FALSE /* print it */);
+
+print_r(array('memory' => (memory_get_usage() - $mem) / (1024 * 1024), 'seconds' => microtime(TRUE) - $time));
